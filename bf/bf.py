b304186ffda51fe66ef7b9ecd54bd2aa9f324865
Aske Bækdal Møller <askebm@live.dk>
2020-10-15 10:58:36 +0200

    It works with breadth first

--- bf/bf.py
+++ bf/bf.py
@@ -17,6 +17,11 @@ class Position():
         x = self.x + other.x
         return Position(x,y)
 
+    def __sub__(self,other):
+        y = self.y - other.y
+        x = self.x - other.x
+        return Position(x,y)
+
     def __eq__(self,other):
         return ( (self.x == other.x) and (self.y == other.y) )
 
@@ -56,11 +61,32 @@ class Tree():
                     self.root.boxes.append(Position(x,y))
                 elif level[y][x] == "G":
                     self.goals.append(Position(x,y))
+
         
 
+        self.solution = [ re.sub("[JM]",".",i) for i in level ]
+        self.solution = [ re.sub("G","J",i) for i in self.solution ]
+        self.solution = "".join(self.solution)
         self.level = [ re.sub("[MJG]",".",i) for i in level ]
         self.root.hash = [ re.sub("G",".",i) for i in level ]
         self.nodes = {"".join(self.root.hash):self.root}
+    def number_of_nodes(self):
+        return len(self.nodes)
+
+    def max_depth(self):
+        node = self.nodes[list(self.nodes)[-1]]
+        cnt = 1
+        while node.parent != None:
+            cnt += 1
+            node = node.parent
+        return cnt
+
+    def isSolution(self,joined_hash):
+        if self.solution == re.sub("M",".",joined_hash):
+            return True
+        else:
+            return False
+
 
     def generate_hash(self,node):
         result = copy.deepcopy(self.level)
@@ -72,7 +98,8 @@ class Tree():
 
     def generate_children(self,node):
 #        print("I wanna make children")
-        for p in [Position(1,0),Position(0,1),Position(-1,0),Position(0,-1)]:
+        moves =[Position(1,0),Position(0,1),Position(-1,0),Position(0,-1)]
+        for p in moves:
             new_player = p+node.player
             behind_box = new_player + p
             n = None
@@ -83,14 +110,23 @@ class Tree():
                 n.boxes = node.boxes
             elif ( node.hash[new_player.y][new_player.x] == "J" and
                         (node.hash[behind_box.y][behind_box.x] == ".")):
-                n = Node()
-                n.player = new_player
-                n.boxes = []
-                for bi in node.boxes:
-                    if new_player == bi:
-                        n.boxes.append(behind_box)
-                    else:
-                        n.boxes.append(bi)
+
+                wall_counter = 0
+                if not [True for g in self.goals if behind_box == g]:
+                    for move in moves:
+                        maybe_wall = move + behind_box
+                        if self.level[maybe_wall.y][maybe_wall.x] == "X":
+                            wall_counter +=1
+
+                if wall_counter <= 1:
+                    n = Node()
+                    n.player = new_player
+                    n.boxes = []
+                    for bi in node.boxes:
+                        if new_player == bi:
+                            n.boxes.append(behind_box)
+                        else:
+                            n.boxes.append(bi)
             
             if n != None:
 #                print("I made a child")
@@ -118,10 +154,7 @@ class Tree():
 #            print("Generated " + str(len(node.children)) +" children")
             for child in node.children:
                 result = True
-                for box in child.boxes:
-                    for g in self.goals:
-                        result &= (g == box)
-                if result:
+                if self.isSolution("".join(child.hash)):
                     return child
             return None
         else:
@@ -149,6 +182,52 @@ class Tree():
             self._to_dot(f,child)
 
 
+    def layer_to_dot(self,path,layer):
+        f = open(path,mode='w')
+        f.write(r'digraph G {'+ "\n")
+        f.write('graph [fontname = "monospace"];\n')
+        f.write('node [fontname = "monospace"];\n')
+        f.write('edge [fontname = "monospace"];\n')
+        self._layer_to_dot(f,self.root,layer)
+        f.write(r'}')
+        f.close()
+
+    def _layer_to_dot(self,f,node,layer):
+        if layer == 0:
+            f.write(r'"' + r'\n'.join(node.hash) + r'"'+ "\n")
+        else:
+            for child in node.children:
+                self._layer_to_dot(f,child,layer-1)
+
+    def generate_path(self,node):
+        parent = node.parent
+        result = ""
+        while parent != None:
+            delta_move = node.player - parent.player
+            instruction = ""
+            if delta_move.x == -1:
+                instruction = "l"
+            elif delta_move.x == 1: 
+                instruction = "r"
+            elif delta_move.y == 1: 
+                instruction = "d"
+            elif delta_move.y == -1: 
+                instruction = "u"
+
+            if node.boxes != parent.boxes:
+                instruction = instruction.upper()
+            result = instruction + result
+
+            node = parent
+            parent = node.parent
+
+
+        return result
+
+
+
+
+
 
 ##
 
@@ -160,9 +239,18 @@ l = ["XXXXXXXXXXXX",
 "X...X...XXXX",
 "XXXXXXXXXXXX"]
 
+##
 
 t = Tree(l)
-t.bf_search(max_iter=135)
+solution = t.bf_search(max_iter=140)
+
+##
+
+t.generate_path(solution)
+
+##
+t.layer_to_dot("layer.gv",108)
+print("Done")
 
 ##
 t.to_dot("dot2.gv")
